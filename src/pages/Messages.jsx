010f30fa827cb6486264bfb97d8d4e89e03e77d3
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2025-01-08 00:48:12 +0530

    work on pages folder Messages section folder chat folder ChatList SideBar Ui

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -1,7 +1,20 @@
+import ChatList from "../section/chat/ChatList";
+import Sidebar from "../section/chat/Sidebar";
+
 const Messages = () => {
-  return <div className="h-screen overflow-hidden">
-    <div className=""></div>
-  </div>;
+  return (
+    <div className="h-screen overflow-hidden">
+      <div className="h-full rounded-sm border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark xl:flex">
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* ChatList */}
+        <ChatList />
+
+        {/* Inbox */}
+      </div>
+    </div>
+  );
 };
 
 export default Messages;
---
+++ src/section/chat/Sidebar.jsx
@@ -0,0 +1,19 @@
+import { Chat, SignOut } from "@phosphor-icons/react";
+
+const Sidebar = () => {
+  return (
+    <div className="flex flex-col border-r border-stroke p-2 dark:border-strokedark">
+      <div className="mx-auto border rounded-md border-stroke p-2 dark:border-strokedark">
+        <Chat size={24} />
+      </div>
+
+      <div className="flex flex-col grow"></div>
+
+      <div className="mx-auto border rounded-md border-stroke p-2 dark:border-strokedark hover:bg-stone-100 hover:cursor-pointer">
+        <SignOut size={24} />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
