e075b0055d481219b4b6635db183d00c743e061c
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2025-01-11 03:58:05 +0530

    working on UI for UserInfo Profile

--- src/section/chat/Inbox.jsx
+++ src/section/chat/Inbox.jsx
@@ -1,177 +1,223 @@
-import { Link, PaperPlaneTilt } from "@phosphor-icons/react";
+import {
+  Link,
+  PaperPlaneTilt,
+  Phone,
+  VideoCamera,
+} from "@phosphor-icons/react";
 import user01 from "../../images/user/user-01.png";
 import Dropdown from "../../components/Dropdown";
 import EmojiPicker from "../../components/EmojiPicker";
+import { useState } from "react";
+import UserInfo from "./UserInfo";
+
 const Inbox = () => {
+  const [userInfoOpen, setUserInfoOpen] = useState(false);
+
+  const handleToggleUserInfo = () => {
+    setUserInfoOpen((prev) => !prev);
+  };
   return (
-    <div className="flex  h-full flex-col border-l border-stroke dark:border-strokedark xl:w-3/4">
-      {/* Chat header */}
-      <div className="sticky flex items-center flex-row justify-between border-b border-stroke dark:border-strokedark px-6 py-4.5">
-        <div className="flex items-center">
-          <div className="mr-4.5 h-13 w-full max-w-13 overflow-hidden rounded-full">
-            <img
-              src={user01}
-              alt="profile"
-              className="h-full w-full object-cover object-center"
-            />
-          </div>
+    <>
+      <div
+        className={`flex  h-full flex-col border-l border-stroke dark:border-strokedark ${
+          userInfoOpen ? "xl:w-1/2" : "xl:w-3/4"
+        } `}
+      >
+        {/* Chat header */}
+        <div className="sticky flex items-center flex-row justify-between border-b border-stroke dark:border-strokedark px-6 py-4.5">
+          <div className="flex items-center" onClick={handleToggleUserInfo}>
+            <div className="mr-4.5 h-13 w-full max-w-13 overflow-hidden rounded-full">
+              <img
+                src={user01}
+                alt="profile"
+                className="h-full w-full object-cover object-center"
+              />
+            </div>
 
-          <div>
-            <h5 className="font-medium text-black dark:text-white">
-              Henry Dholi
-            </h5>
-            <p className="text-xs">Reply to message</p>
-          </div>
-        </div>
-
-        <div>
-          <Dropdown />
-        </div>
-      </div>
-
-      {/* list of messages */}
-
-      <div className="max-h-full spac-y-3.5 overflow-auto no-scrollbar px-6 py-7.5 grow">
-        <div className="max-w-125">
-          <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
-          <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
-            <p>I want to make an appointment tomorrow from 2.00 pm to 5.00?</p>
-          </div>
-          <p className="text-xs">1.55pm</p>
-        </div>
-
-        <div className="max-w-125 ml-auto">
-          <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
-            <p className="text-white">
-              Hello, I will check the schedule and inform
-            </p>
-          </div>
-          <p className="text-xs">2.00pm</p>
-        </div>
-        <div className="max-w-125">
-          <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
-          <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
-            <p>I want to make an appointment tomorrow from 2.00 pm to 5.00?</p>
-          </div>
-          <p className="text-xs">1.55pm</p>
-        </div>
-
-        <div className="max-w-125 ml-auto">
-          <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
-            <p className="text-white">
-              Hello, I will check the schedule and inform
-            </p>
-          </div>
-          <p className="text-xs">2.00pm</p>
-        </div>
-        <div className="max-w-125">
-          <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
-          <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
-            <p>I want to make an appointment tomorrow from 2.00 pm to 5.00?</p>
-          </div>
-          <p className="text-xs">1.55pm</p>
-        </div>
-
-        <div className="max-w-125 ml-auto">
-          <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
-            <p className="text-white">
-              Hello, I will check the schedule and inform
-            </p>
-          </div>
-          <p className="text-xs">2.00pm</p>
-        </div>
-        <div className="max-w-125">
-          <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
-          <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
-            <p>I want to make an appointment tomorrow from 2.00 pm to 5.00?</p>
-          </div>
-          <p className="text-xs">1.55pm</p>
-        </div>
-
-        <div className="max-w-125 ml-auto">
-          <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
-            <p className="text-white">
-              Hello, I will check the schedule and inform
-            </p>
-          </div>
-          <p className="text-xs">2.00pm</p>
-        </div>
-        <div className="max-w-125">
-          <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
-          <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
-            <p>I want to make an appointment tomorrow from 2.00 pm to 5.00?</p>
-          </div>
-          <p className="text-xs">1.55pm</p>
-        </div>
-
-        <div className="max-w-125 ml-auto">
-          <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
-            <p className="text-white">
-              Hello, I will check the schedule and inform
-            </p>
-          </div>
-          <p className="text-xs">2.00pm</p>
-        </div>
-        <div className="max-w-125">
-          <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
-          <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
-            <p>I want to make an appointment tomorrow from 2.00 pm to 5.00?</p>
-          </div>
-          <p className="text-xs">1.55pm</p>
-        </div>
-
-        <div className="max-w-125 ml-auto">
-          <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
-            <p className="text-white">
-              Hello, I will check the schedule and inform
-            </p>
-          </div>
-          <p className="text-xs">2.00pm</p>
-        </div>
-        <div className="max-w-125">
-          <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
-          <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
-            <p>I want to make an appointment tomorrow from 2.00 pm to 5.00?</p>
-          </div>
-          <p className="text-xs">1.55pm</p>
-        </div>
-
-        <div className="max-w-125 ml-auto">
-          <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
-            <p className="text-white">
-              Hello, I will check the schedule and inform
-            </p>
-          </div>
-          <p className="text-xs">2.00pm</p>
-        </div>
-      </div>
-
-      {/* Input field */}
-      <div className="sticky bottom-0 border-t border-stroke bg-white px-6 py-5 dark:border-strokedark dark:bg-boxdark">
-        <form className="flex items-center justify-between space-x-4.5">
-          <div className="relative w-full">
-            <input
-              type="text"
-              placeholder="Type somthing here"
-              className="h-13 w-full rounded-md border border-stroke bg-gray pl-5 pr-19 text-black placeholder-body outline-none focus:border-primary dark:border-strokedark dark:bg-boxdark-2 dark:text-white"
-            />
-
-            <div className="absolute right-5 top-1/2 -translate-y-1/2 items-center  justify-end space-x-4">
-              <button className="hover:text-primary ">
-                <Link size={20} weight="bold" />
-              </button>
-              <button className="hover:text-primary ">
-                <EmojiPicker />
-              </button>
+            <div>
+              <h5 className="font-medium text-black dark:text-white">
+                Henry Dholi
+              </h5>
+              <p className="text-xs">Reply to message</p>
             </div>
           </div>
 
-          <button className="flex items-center justify-center h-13 max-w-13 w-full rounded-md bg-primary text-white hover:bg-opacity-90">
-            <PaperPlaneTilt size={24} />
-          </button>
-        </form>
+          <div className="flex flex-row items-center space-x-5">
+            <button className="text-[#98A6AD] hover:text-body">
+              <VideoCamera size={22} />
+            </button>
+            <button className="text-[#98A6AD] hover:text-body">
+              <Phone size={22} />
+            </button>
+            <Dropdown />
+          </div>
+        </div>
+
+        {/* list of messages */}
+
+        <div className="max-h-full spac-y-3.5 overflow-auto no-scrollbar px-6 py-7.5 grow">
+          <div className="max-w-125">
+            <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
+            <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
+              <p>
+                I want to make an appointment tomorrow from 2.00 pm to 5.00?
+              </p>
+            </div>
+            <p className="text-xs">1.55pm</p>
+          </div>
+
+          <div className="max-w-125 ml-auto">
+            <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
+              <p className="text-white">
+                Hello, I will check the schedule and inform
+              </p>
+            </div>
+            <p className="text-xs">2.00pm</p>
+          </div>
+          <div className="max-w-125">
+            <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
+            <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
+              <p>
+                I want to make an appointment tomorrow from 2.00 pm to 5.00?
+              </p>
+            </div>
+            <p className="text-xs">1.55pm</p>
+          </div>
+
+          <div className="max-w-125 ml-auto">
+            <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
+              <p className="text-white">
+                Hello, I will check the schedule and inform
+              </p>
+            </div>
+            <p className="text-xs">2.00pm</p>
+          </div>
+          <div className="max-w-125">
+            <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
+            <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
+              <p>
+                I want to make an appointment tomorrow from 2.00 pm to 5.00?
+              </p>
+            </div>
+            <p className="text-xs">1.55pm</p>
+          </div>
+
+          <div className="max-w-125 ml-auto">
+            <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
+              <p className="text-white">
+                Hello, I will check the schedule and inform
+              </p>
+            </div>
+            <p className="text-xs">2.00pm</p>
+          </div>
+          <div className="max-w-125">
+            <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
+            <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
+              <p>
+                I want to make an appointment tomorrow from 2.00 pm to 5.00?
+              </p>
+            </div>
+            <p className="text-xs">1.55pm</p>
+          </div>
+
+          <div className="max-w-125 ml-auto">
+            <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
+              <p className="text-white">
+                Hello, I will check the schedule and inform
+              </p>
+            </div>
+            <p className="text-xs">2.00pm</p>
+          </div>
+          <div className="max-w-125">
+            <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
+            <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
+              <p>
+                I want to make an appointment tomorrow from 2.00 pm to 5.00?
+              </p>
+            </div>
+            <p className="text-xs">1.55pm</p>
+          </div>
+
+          <div className="max-w-125 ml-auto">
+            <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
+              <p className="text-white">
+                Hello, I will check the schedule and inform
+              </p>
+            </div>
+            <p className="text-xs">2.00pm</p>
+          </div>
+          <div className="max-w-125">
+            <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
+            <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
+              <p>
+                I want to make an appointment tomorrow from 2.00 pm to 5.00?
+              </p>
+            </div>
+            <p className="text-xs">1.55pm</p>
+          </div>
+
+          <div className="max-w-125 ml-auto">
+            <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
+              <p className="text-white">
+                Hello, I will check the schedule and inform
+              </p>
+            </div>
+            <p className="text-xs">2.00pm</p>
+          </div>
+          <div className="max-w-125">
+            <p className="mb-2.5 text-sm font-medium">Andri Thomas</p>
+            <div className="mb-2.5 rounded-2xl rounded-til-none bg-gray px-5 py-3 dark:bg-boxdark-2">
+              <p>
+                I want to make an appointment tomorrow from 2.00 pm to 5.00?
+              </p>
+            </div>
+            <p className="text-xs">1.55pm</p>
+          </div>
+
+          <div className="max-w-125 ml-auto">
+            <div className="mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3">
+              <p className="text-white">
+                Hello, I will check the schedule and inform
+              </p>
+            </div>
+            <p className="text-xs">2.00pm</p>
+          </div>
+        </div>
+
+        {/* Input field */}
+        <div className="sticky bottom-0 border-t border-stroke bg-white px-6 py-5 dark:border-strokedark dark:bg-boxdark">
+          <form className="flex items-center justify-between space-x-4.5">
+            <div className="relative w-full">
+              <input
+                type="text"
+                placeholder="Type somthing here"
+                className="h-13 w-full rounded-md border border-stroke bg-gray pl-5 pr-19 text-black placeholder-body outline-none focus:border-primary dark:border-strokedark dark:bg-boxdark-2 dark:text-white"
+              />
+
+              <div className="absolute right-5 top-1/2 -translate-y-1/2 items-center  justify-end space-x-4">
+                <button className="hover:text-primary ">
+                  <Link size={20} weight="bold" />
+                </button>
+                <button className="hover:text-primary ">
+                  <EmojiPicker />
+                </button>
+              </div>
+            </div>
+
+            <button className="flex items-center justify-center h-13 max-w-13 w-full rounded-md bg-primary text-white hover:bg-opacity-90">
+              <PaperPlaneTilt size={24} />
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+
+      {/* profile toggle section */}
+      {userInfoOpen && (
+        <div className="w-1/4">
+          <UserInfo handleToggleUserInfo={handleToggleUserInfo} />
+        </div>
+      )}
+    </>
   );
 };
 
---
+++ src/section/chat/UserInfo.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable react/prop-types */
+import {
+  Chat,
+  Clock,
+  DotsThreeVertical,
+  VideoCamera,
+  X,
+} from "@phosphor-icons/react";
+
+const UserInfo = ({ handleToggleUserInfo }) => {
+  return (
+    //full div profile header
+    <div className="border-l flex flex-col h-full border-stroke dark:border-strokedark">
+      <div className="sticky border-b border-stroke dark:border-strokedark flex flex-row items-center justify-between w-full px-6 py-7.5">
+        <div className="text-black dark:text-white text-lg font-semibold">
+          Profile
+        </div>
+
+        <button onClick={handleToggleUserInfo}>
+          <X size={22} weight="bold" />
+        </button>
+      </div>
+
+      {/* Profile */}
+      <div className="mx-auto my-8">
+        <img
+          src="https://images.pexels.com/photos/18189288/pexels-photo-18189288.jpeg?cs=srgb&dl=pexels-toni-guy-pondicherry-404397363-18189288.jpg&fm=jpg&_gl=1*bosmnu*_ga*MTEzMzkzNTk3My4xNzM2NTQ1NzMw*_ga_8JE65Q40S6*MTczNjU0NTcyOS4xLjEuMTczNjU0NTgxNS4wLjAuMA.."
+          alt="profile"
+          className="w-40 h-40 rounded-lg object-cover object-center"
+        />
+      </div>
+
+      {/* profilename */}
+      <div className="px-6 space-y-1 ">
+        <div className="text-black dark:text-white text-lg font-medium">
+          Black Jonathan
+        </div>
+
+        <span className="text-body text-sm font-semibold">sales Manger</span>
+      </div>
+
+      {/* time local */}
+      <div className="px-6 my-4 ">
+        <div className="flex flex-row items-center space-x-2">
+          <Clock size={18} weight="bold" />
+          <div className="text-sm text-body font-semibold">
+            6.50 Am local time
+          </div>
+        </div>
+      </div>
+
+      <div className="px-6 flex flex-row space-x-1 ">
+        <button className="w-full border border-stroke dark:border-strokedark  p-2 rounded-md flex flex-row items-center justify-center">
+          <Chat size={20} className="mr-3" />
+          Message
+        </button>
+
+        <button className="w-full border border-stroke dark:border-strokedark p-2 rounded-md flex flex-row items-center justify-center">
+          <VideoCamera size={20} className="mr-3" />
+          Huddle
+        </button>
+
+        <button className="w-full border border-stroke dark:border-strokedark p-2 rounded-md flex flex-row items-center justify-center">
+          <DotsThreeVertical size={20} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default UserInfo;
