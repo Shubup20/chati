e1bc4992bddaa20c4b2341cc1aa79760eb6b11b7
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2025-01-29 17:21:08 +0530

    set up for Profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const [openTab, setOpenTab] = useState(1);
+  const activeClasses = "text-primary border-primary";
+  const inactiveClasses = "border-transparent";
+  return (
+    <div className="w-full rounded-sm border border-stroke bg-white py-7.5 px-20 shadow-default dark:border-strokedark dark:bg-boxdark">
+      {/* Button for tabs */}
+      <div className="mb-6 flex flex-wrap gap-5 border-b border-stroke dark:border-strokedark sm:gap-10">
+        <Link
+          to="#"
+          onClick={() => {
+            setOpenTab(1);
+          }}
+          className={`border-b-2 py-4 text-sm font-medium hover:text-primary md:text-base ${
+            openTab === 1 ? activeClasses : inactiveClasses
+          } `}
+        >
+          Profile
+        </Link>
+        <Link
+          onClick={() => {
+            setOpenTab(2);
+          }}
+          to="#"
+          className={`border-b-2 py-4 text-sm font-medium hover:text-primary md:text-base ${
+            openTab === 2 ? activeClasses : inactiveClasses
+          } `}
+        >
+          Update password
+        </Link>
+      </div>
+
+      {/* Content for tabs */}
+      <div>
+        <div className={`${openTab === 1 ? "block" : "hidden"}`}>
+          {/* Profile Form */}
+         
+        </div>
+
+        <div className={`${openTab === 2 ? "block" : "hidden"}`}>
+          {/* Update Password */}
+          Update Form
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
